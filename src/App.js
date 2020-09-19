1dcfc18d1333003a59e6236facb78e6ee429a7fc
girishsontakke <girishsontakke7@gmail.com>
2020-09-19 19:51:10 +0530

    components on site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkeForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 20,
+      density: {
+        enable: true,
+        value_area: 200,
+      },
+    },
+  },
+};
 
 class App extends React.Component {
   render() {
-    return <h1> Hello World </h1>;
+    return (
+      <div className="App">
+        <Particles params={particleOptions} className="particles" />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* 
+        
+        <FaceRecognition /> */}
+      </div>
+    );
   }
 }
 
