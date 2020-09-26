6d7dbc7b7ee720eb60f3dae4eaef50abeddde170
girishsontakke <girishsontakke7@gmail.com>
2020-09-26 09:09:31 +0530

    link signIn button to signIn page

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import "./navigation.scss";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <nav className="nav">
-      <Button variant="contained" color="primary">
-        {"Sign In"}
-      </Button>
+      <Link to="/signin">
+        <Button variant="contained" color="primary">
+          {"Sign In"}
+        </Button>
+      </Link>
     </nav>
   );
 };
